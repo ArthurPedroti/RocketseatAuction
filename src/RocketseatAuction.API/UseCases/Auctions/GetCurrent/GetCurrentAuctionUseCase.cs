d06acde0991c7ba16b7aecaf193cf6254f1a650f
Arthur Pedroti <arthurpedroti@gmail.com>
2024-02-15 14:18:26 -0300

    add unit tests

--- src/RocketseatAuction.API/UseCases/Auctions/GetCurrent/GetCurrentAuctionUseCase.cs
+++ src/RocketseatAuction.API/UseCases/Auctions/GetCurrent/GetCurrentAuctionUseCase.cs
@@ -1,20 +1,17 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using RocketseatAuction.API.Contracts;
 using RocketseatAuction.API.Entities;
-using RocketseatAuction.API.Repositories;
 
 namespace RocketseatAuction.API.UseCases.Auctions.GetCurrent;
 
 public class GetCurrentAuctionUseCase
 {
-    public Auction? Execute()
+    private readonly IAuctionRepository _repository;
+
+    public GetCurrentAuctionUseCase(IAuctionRepository repository)
     {
-        var repository = new RocketseatAuctionDbContext();
-
-        var today = DateTime.Now;
-
-        return repository
-            .Auctions
-            .Include(auction => auction.Items)
-            .FirstOrDefault(auction => today >= auction.Starts && today <= auction.Ends);
+        _repository = repository;
     }
+
+    public Auction? Execute() => _repository.GetCurrent();
+    
 }
